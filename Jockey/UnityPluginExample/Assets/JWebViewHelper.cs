7912e6cf1dca2c2f05c9bb6af57397c9ba5c718b
tin.nguyen <tin.nguyen@joy-entertainment.com>
2016-01-02 20:07:56 +0700

    * add Unity Example

---
+++ Jockey/UnityPluginExample/Assets/JWebViewHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class JWebViewHelper
+{
+
+#if UNITY_IOS
+
+#elif UNITY_ANDROID
+    private static AndroidJavaObject mPlayerActivityContext;
+    private static AndroidJavaClass mBridgeClass;
+    private static bool mIsReady = false;
+    public static void Init()
+    {
+        using (var actClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            mPlayerActivityContext = actClass.GetStatic<AndroidJavaObject>("currentActivity");
+        }
+
+        mBridgeClass = new AndroidJavaClass("webview.joy.com.jokey.joy.JWebHelper");
+        Debug.Log(mPlayerActivityContext);
+        mBridgeClass.CallStatic("SetContext", mPlayerActivityContext);
+
+        mIsReady = true;
+
+        Debug.Log("Test: " + mBridgeClass.CallStatic<string>("WhoAreYou"));
+    }
+
+    public static void ShowWebView(string url)
+    {
+        if (!mIsReady)
+            throw new Exception("JWebViewHelper.Init must be call first");
+
+        mBridgeClass.CallStatic("ShowWebViewPopup", url);
+    }
+#else
+    public static void Init()
+    {
+        Debug.Log("[JWebView] Init");
+    }
+
+    public static void ShowWebView(string url)
+    {
+        Debug.Log("[JWebView] Show URL " + url);
+    }
+#endif
+}
---
+++ Jockey/UnityPluginExample/Assets/TestWebView.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestWebView : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	    JWebViewHelper.Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 400, 200), "Click!"))
+        {
+            JWebViewHelper.ShowWebView("http://hero.jskill.com/");
+        }
+    }
+}
