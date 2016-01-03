4078e15baedc2c0049eed45d163c10f84ceac19f
tin.nguyen <tin.nguyen@joy-entertainment.com>
2016-01-03 18:47:40 +0700

    * add file

---
+++ Jockey/Android/GettingStart/app/src/main/java/com/example/phuochp/getingstart/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.phuochp.getingstart;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.HashMap;
+
+import jwebview.JWebHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final Context context = this;
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                Intent intent = new Intent(context, WebViewActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        FloatingActionButton fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+
+                ShowWebViewPopup("http://dev1.moba.jskill.com/TestWebView/index.html");
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void ShowWebViewPopup(String url){
+        JWebHelper.IsUnityMode = false;
+
+
+        JWebHelper.SetContext(this);
+        JWebHelper.ShowWebViewPopup(url);
+
+        JWebHelper.RegisterEvent("log", "JServerManager", "OnWebViewLog");
+
+        test();
+    }
+
+    private void test() {
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        Log.i("tag", "This'll run 3000 milliseconds later");
+                        String hex = "FF0000";
+                        JWebHelper.SendEvent("color-change", "{ \"color\": \"FF0000\" }");
+
+                        JWebHelper.SendEvent("show-image","{}");
+                    }
+                },
+                3000);
+    }
+}
