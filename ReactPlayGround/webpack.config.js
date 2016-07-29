7e448bb551f2f3be58ba69da66aa30ee24931064
tinnguyenhuuletrong <tinnguyenhuuletrong@gmail.com>
2016-07-29 17:50:54 +0700

    * basic config hot reload webpack ( only work with JS and JSX, Not ready for Scss )

--- ReactPlayGround/webpack.config.js
+++ ReactPlayGround/webpack.config.js
@@ -27,8 +27,7 @@ module.exports = {
       {
         test: /(\.js|\.jsx)$/,
         exclude: /(node_modules)/,
-        loader: 'babel',
-        query: { presets: ['es2015', 'stage-0', 'react'] }
+        loaders: ['babel-loader']
       }, {
         test: /(\.scss|\.css)$/,
         loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass')
