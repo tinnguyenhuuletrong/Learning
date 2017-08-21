3f29eea9da3f57bfda3598ec90d95accb868b078
Tin Nguyen <tinnguyenhuuletrong@gmail.com>
2017-08-21 23:50:45 +0700

    [cw] expression check bin mod 7 (part 4)

--- cw/dfa_bin_mod7.js
+++ cw/dfa_bin_mod7.js
@@ -1,6 +1,6 @@
-const exp = /^(0*((1(0(11|0)(111)*0)*1(01*00)*1)|(10101)+)*)+/
+const exp = /^(0*((1(0(11|0)(111)*0)*1(01*00)*1)|(1(0|(0000)+)(01)*101)+)*)+$/;
 
-const inp = "1"
-const res = inp.match(exp)
+const inp = "10010011" // 147 
+const res = exp.test(inp)
 console.log(res)
 
