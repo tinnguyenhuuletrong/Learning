66841c1128222cab3c84951e1aa594b5d5f83630
Tin Nguyen <tinnguyenhuuletrong@gmail.com>
2017-09-05 22:37:23 +0700

    [cw] complete sqrEncode

--- cw/c++/src/problem/sqrEncode.hpp
+++ cw/c++/src/problem/sqrEncode.hpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
 #include <regex>
 #include <cmath>
 
@@ -8,40 +10,72 @@ class CodeSqStrings
 public:
     static std::string code(const std::string &strng);
     static std::string decode(const std::string &strng);
-    
-private:
-    static unsigned int nearestSquareLenge(unsigned int len);
-
 };
 
-unsigned int CodeSqStrings::nearestSquareLenge(unsigned int len) {
-    return ceil(sqrt(len));
-}
+const char PADDING_CHAR = '\013';
 
 std::string CodeSqStrings::code(const std::string &strng) {
     
     std::string input(strng);
     
-    unsigned int n = nearestSquareLenge(input.length());
-    std::regex r("(.{" + std::to_string(n) + "})");
+    unsigned int n = ceil(sqrt((input.length())));
+    std::regex r("(.{" + std::to_string(n) + "})", std::regex_constants::extended);
     
-    std::string sqrString = input.append( fmax(n*n - input.length(), 0), (char)11);
-    sqrString = std::regex_replace (sqrString,r,"$1\n");
+    // padding char 11
+    std::string sqrString = input.append( fmax(n*n - input.length(), 0), PADDING_CHAR);
     
-    std::cout << sqrString << std::endl;
+    // split every n char
+    std::vector<std::string> tokens;
+    std::smatch sm;
+    while(std::regex_search(sqrString, sm, r))
+    {
+        tokens.push_back(sm[0]);
+        sqrString = sm.suffix();
+    }
     
+    // rotate clockwise 90
     std::string res;
+    for (unsigned i =0; i<n; i++) {
+        for (unsigned j=0; j<n; j++) {
+            res.append(&tokens[n-j-1][i], 1);
+        }
+        if(i<n-1)
+            res.append("\n");
+    }
     
-    
-	return "";
+    return res;
 }
 
 std::string CodeSqStrings::decode(const std::string &strng) {
-	return "";
+    std::stringstream ss;
+    ss.str(strng);
+    std::string item;
+    std::vector<std::string> tokens;
+    while (std::getline(ss, item, '\n')) {
+        tokens.push_back(item);
+    }
+    
+    int n = tokens.size();
+    std::string res;
+    for (unsigned i =0; i<n; i++) {
+        for (unsigned j=0; j<n; j++) {
+            if(((int)tokens[j][n - i - 1]) != PADDING_CHAR)
+                res.append(&tokens[j][n - i - 1], 1);
+        }
+    }
+
+    
+    return res;
 }
 
 
 void test() {
-    std::string inp = "I.was.going.fishing.that.morning.at.ten.o'clock";
-    CodeSqStrings::code(inp);
+    std::string inp = "What do you remember? When I looked at his streaky glasses, I wanted "
+    "to leave him. And before that? He stole those cherries for me at midnight. We were walking "
+    "in the rain and I loved him. And before that? I saw him coming "
+    "toward me that time at the picnic, edgy, foreign.";
+    std::string out = CodeSqStrings::code(inp);
+    std::cout << "Encode: " <<out << std::endl;
+    std::string decode = CodeSqStrings::decode(out);
+    std::cout << "Decode: " <<decode << std::endl;
 }
