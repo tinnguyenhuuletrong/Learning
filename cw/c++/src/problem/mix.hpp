77fce88256f4307d133ee8c407f29596f652129e
Tin Nguyen <tinnguyenhuuletrong@gmail.com>
2017-09-18 22:04:10 +0700

    [cw] mix complete

--- cw/c++/src/problem/mix.hpp
+++ cw/c++/src/problem/mix.hpp
@@ -31,53 +31,82 @@ map<char,int> Mix::process(const std::string &s1) {
         res[key] = count + 1;
     }
     
+    map<char,int> trimRes;
+    
     // trim itm <=1
     for(map<char,int>::iterator i = res.begin(); i != res.end(); i++) {
-        if(i->second <=1){
-            i = res.erase(i);
-            i--;
+        if(i->second > 1){
+            trimRes[i->first] = i->second;
         }
     }
     
-    return res;
+    return trimRes;
 }
 
 std::string Mix::mix(const std::string &s1, const std::string &s2)
 {
-    vector<pair<int, pair<int, string>>> mix;
+    vector<string> mix;
     
     // mix
-    auto tmp = process(s1);
-    for(auto i : tmp) {
-        mix.push_back(make_pair(i.second, make_pair(1, string(i.second, i.first))));
+    auto tmp1 = process(s1);
+    for(auto i : tmp1) {
+        string val = "1:" + string(i.second, i.first);
+        mix.push_back(val);
     }
     
-    tmp = process(s2);
-    for(auto i : tmp) {
-        mix.push_back(make_pair(i.second, make_pair(2, string(i.second, i.first))));
+    auto tmp2 = process(s2);
+    for(auto i : tmp2) {
+        string val = string(i.second, i.first);
+        auto combineItm = find(mix.begin(), mix.end(), "1:" + val);
+        if(combineItm != mix.end())
+        {
+            *combineItm = "=:" + val;
+        }
+        else
+        {
+            auto score1 = tmp1.find(i.first);
+            
+            // does not exit in S1
+            if(score1 == tmp1.end())
+            {
+                mix.push_back("2:" + val);
+            }
+            // Exit both S1 and S2 AND frequency S2 larger
+            else if(score1->second < i.second)
+            {
+                auto prev = find(mix.begin(), mix.end(), "1:" + string(score1->second, i.first));
+                mix.erase(prev);
+                mix.push_back("2:" + val);
+            }
+        }
     }
     
     
     // sort
     std::sort(mix.begin(), mix.end(), [ ]( const auto& lhs, const auto& rhs )
     {
-        if(lhs.first == rhs.first) {
-            return lhs.second.second[0] < rhs.second.second[0];
+        if(lhs.length() == rhs.length())
+        {
+            return lhs < rhs;
         }
-        return lhs.first > rhs.first;
+        return lhs.length() > rhs.length();
     });
     
     
-    for(auto i : mix) {
-        cout << i.first << " " << i.second.first << ":" << i.second.second << endl;
+    // build res
+    string res = "";
+    for (int i = 0; i < mix.size(); i++) {
+        res += mix[i];
+        if(i!= mix.size() - 1)
+            res += "/";
     }
     
-    return "";
+    return res;
 }
 
 void test() {
-    string s1 = "Are the kids at home? aaaaa fffff";
-    string s2 = "Yes they are here! aaaaa fffff";
+    string s1 = "Are they here";
+    string s2 = "yes, they are here";
     
     //"=:aaaaaa/2:eeeee/=:fffff/1:tt/2:rr/=:hh"
     cout << Mix::mix(s1,s2) << endl;
