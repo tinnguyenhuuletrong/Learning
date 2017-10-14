0acdbe05946932ead9a97569dae66fa63529c1be
Tin Nguyen <tinnguyenhuuletrong@gmail.com>
2017-10-14 15:44:49 +0700

    [cw] regexParser documents & investigate

--- cw/c++/src/problem/regexParser.hpp
+++ cw/c++/src/problem/regexParser.hpp
@@ -1,27 +1,46 @@
-//------------------ 
+#include <iostream>
+using namespace std;
+
+//------------------
 //	Predefine
 //------------------
-struct RegExp;
+struct RegExp{
+	// dummy
+};
 
-RegExp* any ();
-RegExp* normal (char c);
-RegExp* zeroOrMore (RegExp *starred);
-RegExp* orr (RegExp *left, RegExp *right);
-RegExp* str (RegExp *first);
-RegExp* add (RegExp *str, RegExp *next);
+RegExp* normal (char)                     // ^ A character that is not in "()*|."
+{
+	cout << "normal" << endl;
+}
+RegExp* any ()                            // ^ Any character
+{
+	cout << "any" << endl;
+}
+RegExp* zeroOrMore (RegExp *starred)      // ^ Zero or more occurances of the same regexp
+{
+	cout << "zeroOrMore" << endl;
+}
+RegExp* orr (RegExp* left, RegExp* right) // ^ A choice between 2 regexps
+{
+	cout << "orr" << endl;
+}
+RegExp* str (RegExp *first)               // ^ A sequence of regexps, first element
+{
+	cout << "str" << endl;
+}
+RegExp* add (RegExp *str, RegExp* next)   // ^ A sequence of regexps, additional element
+{
+	cout << "add" << endl;
+}
 
 //------------------
 // Code
 //------------------
 
-#include <iostream>
-using namespace std;
 
 RegExp *parseRegExp (const char *input) {
   return 0;
 }
-
-
 
 void test() {
     
@@ -30,3 +49,22 @@ void test() {
 
 // Ref
 // https://github.com/eliben/code-for-blog/blob/master/2009/regex_fsm/regex_parse.cpp
+//
+// The BNF for our simple regexes is:
+//
+// expr     ::= concat '|' expr
+//          |   concat
+//
+// concat   ::= rep . concat
+//          |   rep
+//
+// rep      ::= atom '*'
+//          |   atom '?'
+//          |   atom
+//
+// atom     ::= chr
+//          |   '(' expr ')'
+//
+// char     ::= alphanumeric character
+//
+
