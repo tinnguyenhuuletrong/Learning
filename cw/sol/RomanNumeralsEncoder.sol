bd9e48e1f60a932a225fd938a5dd326674e61f1d
Tin Nguyen <tinnguyenhuuletrong@gmail.com>
2018-03-10 00:54:36 +0700

    [cw] roman optimize

--- cw/sol/RomanNumeralsEncoder.sol
+++ cw/sol/RomanNumeralsEncoder.sol
@@ -1,31 +1,41 @@
 pragma solidity ^0.4.13;
 
 contract Kata {
-   
-    string[] RomanCharaters = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-    uint[] RomanValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    string[] SYMBOLS = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    uint[] VALUES = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
     function solution(uint n) public returns (string) {
-        uint resultIndex = 0;
+        string memory result = "";
         uint val = n;
         uint i;
-        uint j;
-        uint romanValue;
-        bytes memory results = new bytes(32);
-        for ( i = 0; i < RomanCharaters.length; i++) {
-            romanValue = RomanValue[i];
-            bytes memory _tmpVal = bytes(RomanCharaters[i]);
-            while (val >= romanValue) {
-                for ( j = 0; j < _tmpVal.length; j++) {
-                    results[resultIndex++] = _tmpVal[j];
-                }
-                val -= romanValue;
+        for ( i = 0; i < 13; i++) {
+            uint tmpVal = VALUES[i];
+            while (val >= tmpVal) {
+                result = concat(result, SYMBOLS[i]);
+                val -= tmpVal;
             }
         }
-        bytes memory bytesStringTrimmed = new bytes(resultIndex);
-        for (j = 0; j < resultIndex; j++) {
-            bytesStringTrimmed[j] = results[j];
+        return result;
+    }
+
+    function concat(string _base, string _value) internal returns (string) {
+        bytes memory _baseBytes = bytes(_base);
+        bytes memory _valueBytes = bytes(_value);
+
+        string memory _tmpValue = new string(_baseBytes.length + _valueBytes.length);
+        bytes memory _newValue = bytes(_tmpValue);
+
+        uint i;
+        uint j;
+
+        for ( i = 0; i < _baseBytes.length; i++) {
+            _newValue[j++] = _baseBytes[i];
         }
-        return string(bytesStringTrimmed);
+
+        for ( i = 0; i < _valueBytes.length; i++) {
+            _newValue[j++] = _valueBytes[i];
+        }
+
+        return string(_newValue);
     }
 }
