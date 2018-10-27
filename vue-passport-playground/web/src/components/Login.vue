c0af79f4c9f3b16752a28f4c81b35c2709c8c2e4
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2018-10-27 13:23:25 +0700

    init vue + passport playground

---
+++ vue-passport-playground/web/src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h2>Login</h2>
+        <form v-on:submit="login">
+            <input type="text" name="email" /><br>
+            <input type="password" name="password" /><br>
+            <input type="submit" value="Login" />
+        </form>
+    </div>
+</template>
+
+<script>
+import router from '../router'
+import axios from 'axios'
+export default {
+  name: 'Login',
+  methods: {
+    login: (e) => {
+      e.preventDefault()
+      let email = e.target.elements.email.value
+      let password = e.target.elements.password.value
+      let login = () => {
+        let data = {
+          email: email,
+          password: password
+        }
+        axios.post('/api/login', data)
+          .then((response) => {
+            console.log('Logged in')
+            router.push('/dashboard')
+          })
+          .catch((errors) => {
+            console.log('Cannot log in')
+          })
+      }
+      login()
+    }
+  }
+}
+</script>
