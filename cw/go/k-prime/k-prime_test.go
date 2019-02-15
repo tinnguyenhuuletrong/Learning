2a4311835301daa031628d6891298f4e825353f2
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2019-02-15 21:04:26 +0700

    [cw] v2

--- cw/go/k-prime/k-prime_test.go
+++ cw/go/k-prime/k-prime_test.go
@@ -62,10 +62,6 @@ func primeFactorCount(n int) int {
 		if tmp%start == 0 {
 			count++
 			tmp /= start
-			if val, ok := CacheKPrime[tmp]; ok {
-				count += val
-				break
-			}
 		} else {
 			start = nextPrimeLessThan(start+1, tmp)
 			if start == -1 {
@@ -95,7 +91,7 @@ func Puzzle(s int) int {
 	var count = 0
 	var Set1, Set3, Set7 []int
 
-	for i := 1; i <= s; i++ {
+	for i := 2; i <= s; i++ {
 		var count = primeFactorCount(i)
 		if count == 1 {
 			Set1 = append(Set1, i)
@@ -108,10 +104,12 @@ func Puzzle(s int) int {
 
 	for i := 0; i < len(Set7); i++ {
 		var remain1 = s - Set7[i]
-		for j := 0; j < len(Set3) && Set3[j] <= remain1; j++ {
+		for j := 0; Set3[j] <= remain1; j++ {
 			var remain2 = remain1 - Set3[j]
-			for k := 0; k < len(Set1) && Set1[k] <= remain2; k++ {
+			for k := 0; Set1[k] <= remain2; k++ {
 				if remain2 == Set1[k] {
+					println(Set7[i], Set3[j], Set1[k], "->", Set7[i]+Set3[j]+Set1[k])
+
 					count++
 				}
 			}
@@ -122,8 +120,15 @@ func Puzzle(s int) int {
 
 // go test -run TestSumEvenFibonacci
 func TestKprime(t *testing.T) {
-	var k, start, nd = 5, 1000, 1100
+	// var k, start, nd = 5, 1000, 1100
 	//[]int{1020, 1026, 1032, 1044, 1050, 1053, 1064,   1072, 1092, 1100}
-	fmt.Println(CountKprimes(k, start, nd))
-	fmt.Println(Puzzle(138))
+	// fmt.Println("1", CountKprimes(5, 1000, 1100))
+	// fmt.Println("2", CountKprimes(5, 500, 600))
+	// fmt.Println("3", CountKprimes(7, 1000, 1500))
+	// fmt.Println("4", CountKprimes(7, 10000, 10100))
+	// fmt.Println("5", CountKprimes(7, 100000, 100100))
+	// fmt.Println("6", CountKprimes(12, 100000, 100100))
+	// fmt.Println("7", CountKprimes(1, 2, 30))
+	fmt.Println("8", CountKprimes(8, 10000000, 10000200))
+	fmt.Println(Puzzle(171))
 }
