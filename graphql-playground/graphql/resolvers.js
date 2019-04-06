1d47fb71ae3d8d1e9ad15409b810373b60fbe06b
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2019-04-06 22:19:49 +0700

    [graphql-playground] part2 mutation

--- graphql-playground/graphql/resolvers.js
+++ graphql-playground/graphql/resolvers.js
@@ -1,18 +1,43 @@
 const { Books, Authors } = require('./data')
 const { find, filter } = require('lodash')
+const { UserInputError } = require('apollo-server-express')
 
 const resolvers = {
+  // Query
   Query: {
     books: async () => Books,
     bookById: async (parent, args, context, info) =>
       find(Books, { id: args.id }),
     authors: async () => Authors
   },
+
+  // Model
   Book: {
     author: async book => find(Authors, { id: book.authorId })
   },
   Author: {
     books: async author => filter(Books, { authorId: author.id })
+  },
+
+  // Mutation
+  Mutation: {
+    addBook: async (parent, args, context, info) => {
+      const { title, authorId } = args
+      const authorObj = find(Authors, { id: authorId })
+      if (!authorObj)
+        throw new UserInputError('Invalid authorId', {
+          invalidArgs: {
+            authorId: 'not found'
+          }
+        })
+      const newBook = {
+        id: Books.length + 1,
+        title,
+        authorId
+      }
+      Books.push(newBook)
+      return newBook
+    }
   }
 }
 
