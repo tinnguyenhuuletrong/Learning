6f14386ec1625a094928234916625af1ecec78f7
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2019-08-25 21:51:36 +0700

    sync

---
+++ docs/webrtc-playground/precache-manifest.7e54283d63cb86f758107fe4c67b9bb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4556a14c38a63114ae316fa6559756",
+    "url": "/Learning/webrtc-playground/index.html"
+  },
+  {
+    "revision": "22e62dbce9268fb3b8d6",
+    "url": "/Learning/webrtc-playground/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "22e62dbce9268fb3b8d6",
+    "url": "/Learning/webrtc-playground/static/js/2.35b514fa.chunk.js"
+  },
+  {
+    "revision": "fec7a636145db8058fd1",
+    "url": "/Learning/webrtc-playground/static/js/main.40cbf93b.chunk.js"
+  },
+  {
+    "revision": "657a827d36d2fecaf3b8",
+    "url": "/Learning/webrtc-playground/static/js/runtime~main.5ec6b0ce.js"
+  }
+]);
--- docs/webrtc-playground/service-worker.js
+++ docs/webrtc-playground/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Learning/webrtc-playground/precache-manifest.700032a05565a264024de641b11cca62.js"
+  "/Learning/webrtc-playground/precache-manifest.7e54283d63cb86f758107fe4c67b9bb7.js"
 );
 
 self.addEventListener('message', (event) => {
