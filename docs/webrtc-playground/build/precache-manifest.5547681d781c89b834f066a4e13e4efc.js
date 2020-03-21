78bb61da10911ff43799891dbd5a1ca81d350ef7
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2020-03-21 20:10:11 +0700

    sync

---
+++ docs/webrtc-playground/build/precache-manifest.5547681d781c89b834f066a4e13e4efc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f5897e033924b4684bfdee48a927dc4",
+    "url": "/Learning/webrtc-playground/index.html"
+  },
+  {
+    "revision": "79ffed9038691453da38",
+    "url": "/Learning/webrtc-playground/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "79ffed9038691453da38",
+    "url": "/Learning/webrtc-playground/static/js/2.027b6874.chunk.js"
+  },
+  {
+    "revision": "ad2c73ff3c84b772d7ee",
+    "url": "/Learning/webrtc-playground/static/js/main.8b9179a8.chunk.js"
+  },
+  {
+    "revision": "657a827d36d2fecaf3b8",
+    "url": "/Learning/webrtc-playground/static/js/runtime~main.5ec6b0ce.js"
+  }
+]);
