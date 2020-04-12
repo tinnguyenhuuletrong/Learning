d549eb8317760e2e6ab3d28cba940385b3f26fb5
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2020-04-12 18:22:53 +0700

    [anim] play

--- react-web-animation/play-one/src/App.js
+++ react-web-animation/play-one/src/App.js
@@ -1,32 +1,57 @@
-import React, { useState, useRef } from "react";
+import React, { useRef, useState, useLayoutEffect, useMemo } from "react";
 import { Grid, Paper, Typography } from "@material-ui/core";
 import { blue, green } from "@material-ui/core/colors";
 
 import { useSpring, animated, interpolate } from "react-spring";
 import { useDrag } from "react-use-gesture";
+import useKeyPress from "./useKeyPress";
 
 import "./app.css";
 
-const Styles = {};
+function swap(arr, i, j) {
+  const tmp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tmp;
+  return arr;
+}
 
-const Card = ({ id }) => {
+function contain(cx, cy, { x, y, width, height }) {
+  return x <= cx && cx <= x + width && y <= cy && cy <= y + height;
+}
+
+const Styles = {};
+const Card = ({ id, index, context }) => {
   const [{ x, y, distance }, set] = useSpring(() => ({
     x: 0,
     y: 0,
     distance: 0,
   }));
   const [isDown, setIsDown] = useState(false);
-  const dragHandler = useDrag(({ down, movement: [mx, my], distance }) => {
-    setIsDown(down);
-    set({
-      x: down ? mx : 0,
-      y: down ? my : 0,
-      distance: down ? distance : 0,
-    });
-  });
+  const ref = useRef(null);
+  const dragHandler = useDrag(
+    ({ down, values, movement: [mx, my], distance, cancel }) => {
+      setIsDown(down);
+      set({
+        x: down ? mx : 0,
+        y: down ? my : 0,
+        distance: down ? distance : 0,
+      });
+
+      const newIndex = context.calculateNewIndex(values[0], values[1]);
+      if (newIndex !== -1 && newIndex !== index) {
+        context.doSwap(index, newIndex);
+        cancel();
+      }
+    }
+  );
+
+  useLayoutEffect(() => {
+    context.updateBoundRect(id, ref.current.getBoundingClientRect());
+  }, [id, context]);
 
   return (
     <Paper
+      ref={ref}
       component={animated.div}
       {...dragHandler()}
       style={{
@@ -54,7 +79,41 @@ const Card = ({ id }) => {
 };
 
 const App = () => {
-  const items = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
+  const [items, setItems] = useState([
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+  ]);
+
+  useKeyPress("1", () => {
+    swap(items, 0, 1);
+    setItems(items);
+  });
+
+  const context = useMemo(() => {
+    return {
+      updateBoundRect: (id, rec) => {
+        const itm = items.find((itm) => itm.id === id);
+        itm.boundRect = rec;
+      },
+      calculateNewIndex: (x, y) => {
+        const index = items.findIndex(({ id, boundRect }) => {
+          return boundRect && contain(x, y, boundRect);
+        });
+        return index;
+      },
+      doSwap: (index1, index2) => {
+        console.log("swap", index1, index2);
+        swap(items, index1, index2);
+        console.log(items.map((itm) => itm.id));
+        setItems([...items]);
+      },
+    };
+  }, [items]);
+
+  window.items = items;
+
   return (
     <Grid
       container
@@ -63,8 +122,8 @@ const App = () => {
       alignItems="center"
       style={Styles.container}
     >
-      {items.map(({ id }) => (
-        <Card key={id} id={id} />
+      {items.map(({ id }, index) => (
+        <Card key={id} index={index} id={id} context={context} />
       ))}
     </Grid>
   );
