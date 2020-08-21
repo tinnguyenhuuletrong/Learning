8260bd284d4cb4383cd6edd5bbba4cab4429795d
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2020-08-21 15:34:22 +0700

    [graphql] hasura

---
+++ GraphQL/hasura/knexfile.js
@@ -0,0 +1,40 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: "postgres://postgres:postgrespassword@localhost:5432/postgres",
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
