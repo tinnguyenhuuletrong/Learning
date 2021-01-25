7f0461a1fed2ac10a49241e08ea00c3eed35f0d7
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2021-01-25 23:39:46 +0700

    protobuf js play

--- protobuf_playground/node-protobufjs/play.js
+++ protobuf_playground/node-protobufjs/play.js
@@ -1,15 +1,17 @@
 const protobuf = require("protobufjs");
+const fs = require("fs");
 
 async function main() {
   const root = await protobuf.load("./sample.proto");
 
   const AwesomeMessage = root.lookupType("AwesomeMessage");
+  const img = fs.readFileSync("./mrz_passport_3.jpeg");
 
   const payload = {
     strField: "hello",
     floatField: 1.5,
     intField: 10,
-    bufferField: Buffer.from("abcd"),
+    bufferField: img,
   };
 
   const err = AwesomeMessage.verify(payload);
@@ -18,6 +20,8 @@ async function main() {
   const msg = AwesomeMessage.create(payload);
   const buffer = AwesomeMessage.encode(msg).finish();
 
+  console.log("original size:", img.length);
+  console.log("encoded size:", buffer.length);
   console.log("encoded:", buffer);
 
   const decodedMsg = AwesomeMessage.decode(buffer);
