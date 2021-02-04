54ae710c5abd9b207e073e6c23de2df38d1b9bd7
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2021-02-04 15:38:33 +0700

    opencv c++

---
+++ opencv-2021-playground/c++/DisplayVideo.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char **argv){
+  if (argc != 2)
+  {
+    std::cout << ("usage: DisplayVideo.out <Video_Path>\n");
+    return -1;
+  }
+
+  // Create a VideoCapture object and open the input file
+  // If the input is the web camera, pass 0 instead of the video file name
+  VideoCapture cap(argv[1]); 
+   
+  // Check if camera opened successfully
+  if(!cap.isOpened()){
+    std::cout << "Error opening video stream or file" << std::endl;
+    return -1;
+  }
+	
+  while(1){
+    Mat frame;
+    // Capture frame-by-frame
+    cap >> frame;
+ 
+    // If the frame is empty, break immediately
+    if (frame.empty())
+      break;
+
+    // Display the resulting frame
+    imshow( "Frame", frame );
+
+    // Press  ESC on keyboard to exit
+    char c=(char)waitKey(25);
+    if(c==27)
+      break;
+  }
+ 
+  // When everything done, release the video capture object
+  cap.release();
+  // Closes all the frames
+  destroyAllWindows();
+  return 0;
+}
