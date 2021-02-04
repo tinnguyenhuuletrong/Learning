2df830dfd9e28e9c4f953ff961b5db125a2df006
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2021-02-04 22:34:13 +0700

    with webcam

--- opencv-2021-playground/c++/DisplayVideo.cpp
+++ opencv-2021-playground/c++/DisplayVideo.cpp
@@ -7,13 +7,16 @@ using namespace std;
 int main(int argc, char **argv){
   if (argc != 2)
   {
-    std::cout << ("usage: DisplayVideo.out <Video_Path>\n");
+    std::cout << ("usage: DisplayVideo.out <Video_Path>|'webcam'\n");
     return -1;
   }
 
+  std::string mode(argv[1]);  
+  bool isWebcam = mode == "webcam";
+
   // Create a VideoCapture object and open the input file
   // If the input is the web camera, pass 0 instead of the video file name
-  VideoCapture cap(argv[1]); 
+  VideoCapture cap = isWebcam ? VideoCapture(0) : VideoCapture(mode.c_str()); 
    
   // Check if camera opened successfully
   if(!cap.isOpened()){
