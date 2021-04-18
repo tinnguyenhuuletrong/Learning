5f3f36c5e293ea5f24f7fee7eb420518b742ea52
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2021-04-18 23:34:52 +0700

    [opencv][python] train and  face detect

--- opencv-2021-playground/python/module.py
+++ opencv-2021-playground/python/module.py
@@ -1,5 +1,7 @@
 from fastapi import HTTPException
+from sklearn.neighbors import LocalOutlierFactor
 import face_recognition
+import numpy as np
 import cv2
 
 threshold = 0.6
@@ -31,3 +33,63 @@ def face_process(path):
       "distance": face_distance
     }
   }
+
+
+def video_process(file):
+  cap = cv2.VideoCapture(file)
+  faces = []
+  frameCount = 0
+  print("Step 1: Detect faces")
+
+  while (cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == True:
+      # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_frame = frame[:, :, ::-1]
+
+        face_locations = face_recognition.face_locations(rgb_frame, number_of_times_to_upsample=0, model="cnn")
+        if len(face_locations) <= 0: continue
+
+        face_encodings = face_recognition.face_encodings(rgb_frame, face_locations, model="large")
+        for enc in face_encodings:
+          faces.append(enc)
+
+        frameCount += 1
+        print(frameCount)
+    else:
+      break
+  
+  print("Step 2: Detected %d face(s). Train model" %len(faces))
+
+  X_train = faces
+  X_test = faces
+  model = LocalOutlierFactor(novelty=True)
+  model.fit(X_train)
+
+  test_res = model.predict(X_test)
+  print("Step 3: Test result", test_res)
+  return model, faces
+
+def face_detect(model, byte):
+  rgb_frame = load_image_into_numpy_array(byte)
+  face_locations = face_recognition.face_locations(rgb_frame)
+
+  # no faces
+  if len(face_locations) <= 0: 
+    return {
+      "face_locations": [],
+      "results": []
+    }
+  face_encodings = face_recognition.face_encodings(rgb_frame, face_locations, model="large")
+
+  res = model.predict(face_encodings)
+  return {
+    "face_locations": face_locations,
+    "results": res.tolist()
+  }
+
+def load_image_into_numpy_array(data):
+    npimg = np.frombuffer(data, np.uint8)
+    frame = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+    cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    return frame
