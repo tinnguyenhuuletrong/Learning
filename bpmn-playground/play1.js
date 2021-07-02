b5e453347a1d107eadc5d6c69d95647c4dc438cd
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2021-07-02 18:54:09 +0700

    [bpmn] camuda ext

--- bpmn-playground/play1.js
+++ bpmn-playground/play1.js
@@ -1,23 +1,60 @@
-process.env.DEBUG = 'bpmn-engine:*'
+process.env.DEBUG = "bpmn-engine:*";
 
-const { Engine } = require('bpmn-engine')
-const fs = require('fs')
+const Bpmn = require("bpmn-engine");
+const fs = require("fs");
 
-const engine = Engine({
-  name: 'using variables',
-  source: fs.readFileSync('./model/simple-task.bpmn')
-})
 
-const variables = {
-  input: 10
+function ServiceExpression (activity) {
+  const { type: atype, behaviour, environment } = activity;
+  const expression = behaviour.expression;
+
+  const type = `${atype}:expression`;
+
+  return {
+    type,
+    expression,
+    execute,
+  };
+
+  function execute (executionMessage, callback) {
+    const serviceFn = environment.resolveExpression(expression, executionMessage);
+    serviceFn.call(activity, executionMessage, (err, result) => {
+      callback(err, result);
+    });
+  }
 }
 
-engine.execute(
-  {
-    variables
-  },
-  (err, engineApi) => {
-    if (err) throw err
-    console.log('completed')
-  }
-)
+async function main () {
+  const engine = Bpmn.Engine({
+    name: "simple flow",
+    source: fs.readFileSync("./model/simple.bpmn"),
+    moddleOptions: {
+      camunda: require('camunda-bpmn-moddle/resources/camunda')
+    },
+    services: {
+      echo: (scope, callback) => {
+        console.log("echo", scope);
+        callback(null, { data: 'hello from node' })
+      },
+    },
+    extensions: {
+      camundaServiceTask (activity) {
+        if (activity.behaviour.expression) {
+          activity.behaviour.Service = ServiceExpression;
+        }
+        if (activity.behaviour.resultVariable) {
+          activity.on('end', (api) => {
+            activity.environment.output[activity.behaviour.resultVariable] = api.content.output;
+          });
+        }
+      },
+    }
+  });
+
+  const variables = {};
+  const api = await engine.execute({});
+
+  console.log(api.environment.output)
+}
+
+main();
---
+++ bpmn-playground/play2.js
@@ -0,0 +1,88 @@
+// process.env.DEBUG = "bpmn-engine:*";
+
+const EventEmitter = require("events").EventEmitter;
+const Bpmn = require("bpmn-engine");
+const fs = require("fs");
+
+function ServiceExpression(activity) {
+  const { type: atype, behaviour, environment } = activity;
+  const expression = behaviour.expression;
+
+  const type = `${atype}:expression`;
+
+  return {
+    type,
+    expression,
+    execute,
+  };
+
+  function execute(executionMessage, callback) {
+    const serviceFn = environment.resolveExpression(
+      expression,
+      executionMessage
+    );
+
+    executionMessage.environment = environment;
+    serviceFn.call(activity, executionMessage, (err, result) => {
+      callback(err, result);
+    });
+  }
+}
+
+async function main() {
+  const engine = Bpmn.Engine({
+    name: "payment process",
+    source: fs.readFileSync("./model/payment.bpmn"),
+    moddleOptions: {
+      camunda: require("camunda-bpmn-moddle/resources/camunda"),
+    },
+    services: {
+      doPayment: (scope, callback) => {
+        const { pid, pvalue = 2000 } = scope.environment.variables;
+        console.log("doPayment", { pid, pvalue });
+        callback(null, { value: pvalue });
+      },
+      doShipWithInsurance: (scope, callback) => {
+        callback(null, { shipType: "safe" });
+      },
+      doShipWithoutInsurance: (scope, callback) => {
+        callback(null, { shipType: "unsafe" });
+      },
+      gt(v1, v2) {
+        console.log("gt", v1, v2);
+        return v1 > v2;
+      },
+    },
+    extensions: {
+      camundaServiceTask(activity) {
+        if (activity.behaviour.expression) {
+          activity.behaviour.Service = ServiceExpression;
+        }
+        if (activity.behaviour.resultVariable) {
+          activity.on("end", (api) => {
+            activity.environment.output[activity.behaviour.resultVariable] =
+              api.content.output;
+          });
+        }
+      },
+    },
+  });
+
+  const variables = {
+    pid: 1,
+    pvalue: 3000,
+  };
+  const listener = new EventEmitter();
+
+  listener.on("flow.take", (flow) =>
+    console.log(`\tflow <${flow.id}> was taken`)
+  );
+
+  const api = await engine.execute({ variables, listener });
+  console.log("finish", api.state, api.environment.output);
+  console.log("final state", api.getState());
+}
+
+main();
+
+// ${environment.services.gt(environment.output.paymentResult.value, 100)}
