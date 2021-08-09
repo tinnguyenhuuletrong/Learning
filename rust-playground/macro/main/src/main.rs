22c475a11de917c012bcf72563888cdbcd0dd638
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2021-08-09 11:28:49 +0700

    [rust] macro

---
+++ rust-playground/macro/main/src/main.rs
@@ -0,0 +1,11 @@
+extern crate hello_macro_derive;
+
+use hello_macro::HelloMacro;
+use hello_macro_derive::HelloMacro;
+
+#[derive(HelloMacro)]
+struct Pancakes;
+
+fn main() {
+    Pancakes::hello_macro();
+}
