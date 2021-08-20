09b219ba8c10a58f698034c9d36b21c946aae921
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2021-08-20 14:30:43 +0700

    [rust] actor cont

--- rust-playground/actix_playground/examples/arbiter.rs
+++ rust-playground/actix_playground/examples/arbiter.rs
@@ -1,5 +1,4 @@
 use actix::prelude::*;
-use std::time::Duration;
 
 struct SumActor {}
 
@@ -33,36 +32,28 @@ impl Handler<Display> for DisplayActor {
   type Result = ();
 
   fn handle(&mut self, msg: Display, _ctx: &mut Context<Self>) -> Self::Result {
-    let thread_id = std::thread::current().id();
-    println!("{:?} Got {:?}", thread_id, msg.0);
+    println!("Got {:?}", msg.0);
   }
 }
 
 struct HeavyJobActor {}
 
 impl Actor for HeavyJobActor {
-  type Context = SyncContext<Self>;
+  type Context = Context<Self>;
 }
 
-impl Handler<Value> for HeavyJobActor {
+impl Handler<Value> from HeavyJobActor {
   type Result = usize;
-  fn handle(&mut self, msg: Value, _ctx: &mut SyncContext<Self>) -> Self::Result {
-    let thread_id = std::thread::current().id();
-    println!("{:?} HeavyJobActor processing", thread_id);
-    std::thread::sleep(Duration::from_millis(2000));
-    println!("{:?} HeavyJobActor finish {}", thread_id, msg.0 + msg.1);
-    msg.0 + msg.1
+  fn handle(&mut self, msg: Value, _ctx: &mut Context<Self>) -> Self::Result {
+    
   }
 }
 
 fn main() {
   let system = System::new();
 
-  // 10 worker for thread pool
-  let heavy_addr = SyncArbiter::start(10, || HeavyJobActor {});
-
-  // Spawn the future onto the current Arbiter/event loop
-  Arbiter::current().spawn(async move {
+  // Define an execution flow using futures
+  let execution = async {
     // `Actor::start` spawns the `Actor` on the *current* `Arbiter`, which
     // in this case is the System arbiter
     let sum_addr = SumActor {}.start();
@@ -84,24 +75,17 @@ fn main() {
         eprintln!("Encountered mailbox error: {:?}", e);
       }
     };
+  };
 
-    // Call to SyncArbiter ( difference thread )
-    for i in 1..10 {
-      heavy_addr.do_send(Value(i, i));
-    }
-    let sum_result = heavy_addr.send(Value(1, 2)).await;
-    match sum_result {
-      Ok(res) => {
-        // `res` is now the `usize` returned from `SumActor` as a response to `Value(6, 7)`
-        // Once the future is complete, send the successful response (`usize`)
-        // to the `DisplayActor` wrapped in a `Display
-        dis_addr.send(Display(res)).await;
-      }
-      Err(e) => {
-        eprintln!("Encountered mailbox error: {:?}", e);
-      }
-    };
-  });
+  // Spawn the future onto the current Arbiter/event loop
+  Arbiter::current().spawn(execution);
+
+  // We only want to do one computation in this example, so we
+  // shut down the `System` which will stop any Arbiters within
+  // it (including the System Arbiter), which will in turn stop
+  // any Actor Contexts running within those Arbiters, finally
+  // shutting down all Actors.
+  System::current().stop();
 
   system.run();
 }
