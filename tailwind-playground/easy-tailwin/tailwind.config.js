74ed5dd39be461269b1473613ca1f1f7204d5d0f
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2022-01-09 21:25:13 +0700

    easy tailwin

---
+++ tailwind-playground/easy-tailwin/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./style/**/*.{html,js}", "*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6363",
+      },
+    },
+  },
+  plugins: [],
+};
