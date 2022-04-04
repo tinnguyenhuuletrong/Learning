f8739f6bacd8e612e1f144871ba1774ff01484b8
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2022-04-04 16:41:10 +0700

    [flutter]fb_clone part 2

--- flutter-playground/playground_fb_clone/lib/widgets/post_container.dart
+++ flutter-playground/playground_fb_clone/lib/widgets/post_container.dart
@@ -1,5 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:playground_fb_clone/configs/palette.dart';
 import 'package:playground_fb_clone/models/model.dart';
+import 'package:playground_fb_clone/widgets/user_avatar.dart';
 
 class PostContainer extends StatelessWidget {
   final Post post;
@@ -9,12 +12,219 @@ class PostContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
-      height: 100,
-      color: Colors.blue[200],
-      child: Row(
-        children: [],
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                _PostHeader(post: post),
+                const SizedBox(
+                  height: 4,
+                ),
+                Text(post.caption)
+              ],
+            ),
+          ),
+          post.imageUrl != null
+              ? Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                  child: CachedNetworkImage(
+                    imageUrl: post.imageUrl ?? '',
+                  ))
+              : const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 8, 0, 0),
+                  child: SizedBox.shrink(),
+                ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: _PostStats(post: post),
+          )
+        ],
       ),
+    );
+  }
+}
+
+class _PostStats extends StatelessWidget {
+  const _PostStats({
+    Key? key,
+    required this.post,
+  }) : super(key: key);
+
+  final Post post;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(4),
+              decoration: const BoxDecoration(
+                color: Palette.facebookBlue,
+                shape: BoxShape.circle,
+              ),
+              child: const Icon(
+                Icons.thumb_up,
+                size: 10,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            Text(
+              '${post.likes}',
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+            const Spacer(),
+            Text(
+              '${post.comments} Comments',
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            Text(
+              '${post.shares} Shares',
+              style: TextStyle(color: Colors.grey[600]),
+            )
+          ],
+        ),
+        const Divider(),
+        Row(
+          children: [
+            _PostButton(
+              icon: Icon(
+                Icons.thumb_up_alt_outlined,
+                color: Colors.grey[600],
+                size: 20,
+              ),
+              label: "Like",
+              onTap: () {
+                print('Post ${post.hashCode}.On Like');
+              },
+            ),
+            _PostButton(
+              icon: Icon(
+                Icons.comment_outlined,
+                color: Colors.grey[600],
+                size: 20,
+              ),
+              label: "Comment",
+              onTap: () {
+                print('Post ${post.hashCode}.On Comment');
+              },
+            ),
+            _PostButton(
+              icon: Icon(
+                Icons.share_outlined,
+                color: Colors.grey[600],
+                size: 25,
+              ),
+              label: "Share",
+              onTap: () {
+                print('Post ${post.hashCode}.On Share');
+              },
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class _PostButton extends StatelessWidget {
+  final Icon icon;
+  final String label;
+  final GestureTapCallback onTap;
+
+  const _PostButton(
+      {Key? key, required this.icon, required this.label, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Material(
+        color: Colors.white,
+        child: InkWell(
+          onTap: onTap,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            height: 25,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                icon,
+                const SizedBox(width: 4),
+                Text(
+                  label,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _PostHeader extends StatelessWidget {
+  const _PostHeader({
+    Key? key,
+    required this.post,
+  }) : super(key: key);
+
+  final Post post;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        UserAvatar(imageUrl: post.user.imageUrl),
+        const SizedBox(
+          width: 8,
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                post.user.name,
+                style: const TextStyle(fontWeight: FontWeight.w600),
+              ),
+              Row(
+                children: [
+                  Text(
+                    post.timeAgo + " - ",
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Icon(
+                    Icons.public,
+                    size: 12,
+                    color: Colors.grey[600],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.more_horiz),
+        )
+      ],
     );
   }
 }
