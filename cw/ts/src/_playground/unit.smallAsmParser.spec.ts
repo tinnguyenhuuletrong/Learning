c5c05ac7531c95752fe4c33841422a797fe38a64
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2023-06-08 15:01:33 +0700

    update

--- cw/ts/src/_playground/unit.smallAsmParser.spec.ts
+++ cw/ts/src/_playground/unit.smallAsmParser.spec.ts
@@ -2,6 +2,11 @@ import { describe, expect, test } from "@jest/globals";
 import { ASTKinds, parse } from "./smallAsmParser";
 
 describe("playground", () => {
+  test("test 4", () => {
+    let res = parse(" [ x y z ] ( 2*3*x + 5*y - 3*z ) / (1 + 3 + 2*2)");
+    console.dir(res, { depth: 10 });
+    expect(res.errs.length).toBe(0);
+  });
   test("test 3", () => {
     let res = parse(" [ x y ] ( x + y ) / 2");
     console.dir(res, { depth: 10 });
