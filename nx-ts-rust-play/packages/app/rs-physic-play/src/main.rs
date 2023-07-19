94da562093c226a8eeb7645693edebbd64031a8c
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2023-07-19 11:45:37 +0700

    [nx] more physic play

--- nx-ts-rust-play/packages/app/rs-physic-play/src/main.rs
+++ nx-ts-rust-play/packages/app/rs-physic-play/src/main.rs
@@ -1,4 +1,112 @@
+use anyhow::Result;
 use rapier2d::prelude::*;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::Write;
+
+#[derive(Serialize, Deserialize)]
+struct PhysicsState {
+    pub islands: IslandManager,
+    pub broad_phase: BroadPhase,
+    pub narrow_phase: NarrowPhase,
+    pub bodies: RigidBodySet,
+    pub colliders: ColliderSet,
+    pub impulse_joint_set: ImpulseJointSet,
+    pub multibody_joint_set: MultibodyJointSet,
+    pub ccd_solver: CCDSolver,
+    pub query_pipeline: QueryPipeline,
+    pub integration_parameters: IntegrationParameters,
+    pub gravity: Vector<f32>,
+
+    #[serde(skip)]
+    physic_pipeline: PhysicsPipeline,
+}
+
+fn setup_physics_scene(bodies: RigidBodySet, colliders: ColliderSet) -> PhysicsState {
+    let island_manager = IslandManager::new();
+    let broad_phase = BroadPhase::new();
+    let narrow_phase = NarrowPhase::new();
+    let ccd_solver = CCDSolver::new();
+    let impulse_joint_set = ImpulseJointSet::new();
+    let multibody_joint_set = MultibodyJointSet::new();
+    let integration_parameters = IntegrationParameters::default();
+    let gravity = vector![2.0, -9.81];
+    let query_pipeline = QueryPipeline::new();
+
+    PhysicsState {
+        islands: island_manager,
+        broad_phase,
+        narrow_phase,
+        bodies,
+        colliders,
+        impulse_joint_set,
+        multibody_joint_set,
+        ccd_solver,
+        query_pipeline,
+        integration_parameters,
+        gravity,
+
+        physic_pipeline: PhysicsPipeline::new(),
+    }
+}
+
+impl PhysicsState {
+    fn step(
+        mut self: Self,
+        physics_hooks: &impl PhysicsHooks,
+        event_handler: &impl EventHandler,
+    ) -> Self {
+        self.physic_pipeline.step(
+            &self.gravity,
+            &self.integration_parameters,
+            &mut self.islands,
+            &mut self.broad_phase,
+            &mut self.narrow_phase,
+            &mut self.bodies,
+            &mut self.colliders,
+            &mut self.impulse_joint_set,
+            &mut self.multibody_joint_set,
+            &mut self.ccd_solver,
+            Some(&mut self.query_pipeline),
+            physics_hooks,
+            event_handler,
+        );
+
+        self
+    }
+
+    fn get_bodies(self: &Self) -> &RigidBodySet {
+        &self.bodies
+    }
+
+    fn get_colliders(self: &Self) -> &ColliderSet {
+        &self.colliders
+    }
+}
+
+struct EventLog {}
+impl EventHandler for EventLog {
+    fn handle_collision_event(
+        &self,
+        _bodies: &RigidBodySet,
+        _colliders: &ColliderSet,
+        _event: CollisionEvent,
+        _contact_pair: Option<&ContactPair>,
+    ) {
+        println!("\t handle_collision_event: type={:?}", _event)
+    }
+
+    fn handle_contact_force_event(
+        &self,
+        _dt: Real,
+        _bodies: &RigidBodySet,
+        _colliders: &ColliderSet,
+        _contact_pair: &ContactPair,
+        _total_force_magnitude: Real,
+    ) {
+        println!("\t handle_contact_force_event")
+    }
+}
 
 fn main() {
     let mut rigid_body_set = RigidBodySet::new();
@@ -13,49 +121,61 @@ fn main() {
         .translation(vector![0.0, 10.0])
         .build();
 
-    let ball_collider = ColliderBuilder::ball(0.5).restitution(0.7).build();
+    let ball_collider = ColliderBuilder::ball(0.5)
+        .restitution(0.7)
+        .active_events(ActiveEvents::COLLISION_EVENTS)
+        .build();
+
     let ball_body_handle = rigid_body_set.insert(rigid_body_ball);
     let ball_collider_handle =
         collider_set.insert_with_parent(ball_collider, ball_body_handle, &mut rigid_body_set);
 
     /* Create other structures necessary for the simulation. */
-    let gravity = vector![2.0, -9.81];
-    let integration_parameters = IntegrationParameters::default();
-    let mut physics_pipeline = PhysicsPipeline::new();
-    let mut island_manager = IslandManager::new();
-    let mut broad_phase = BroadPhase::new();
-    let mut narrow_phase = NarrowPhase::new();
-    let mut impulse_joint_set = ImpulseJointSet::new();
-    let mut multibody_joint_set = MultibodyJointSet::new();
-    let mut ccd_solver = CCDSolver::new();
+    let mut scene = setup_physics_scene(rigid_body_set, collider_set);
     let physics_hooks = ();
-    let event_handler = ();
+    let event_handler: EventLog = EventLog {};
 
     /* Run the game loop, stepping the simulation once per frame. */
-    for _ in 0..200 {
-        physics_pipeline.step(
-            &gravity,
-            &integration_parameters,
-            &mut island_manager,
-            &mut broad_phase,
-            &mut narrow_phase,
-            &mut rigid_body_set,
-            &mut collider_set,
-            &mut impulse_joint_set,
-            &mut multibody_joint_set,
-            &mut ccd_solver,
-            None,
-            &physics_hooks,
-            &event_handler,
-        );
+    for it in 0..200 {
+        scene = scene.step(&physics_hooks, &event_handler);
 
-        let ball_body = &rigid_body_set[ball_body_handle];
+        let ball_body = scene.get_bodies().get(ball_body_handle).unwrap();
         let trans = ball_body.translation();
-        println!("Ball pos: {} - {}", trans.x, trans.y);
+        println!("It {} : Ball pos: {} - {}", it, trans.x, trans.y);
     }
 
-    // Shape -> 2D points
-    let tmp = collider_set
+    play_with_collider_shape(&scene, ground_collider_handle, ball_collider_handle);
+    play_with_serialize(&scene).unwrap();
+}
+
+fn play_with_serialize(scene: &PhysicsState) -> Result<()> {
+    let serialized = bincode::serialize(scene)?;
+    let hash = sha256::digest(&serialized);
+
+    let mut f = File::create("./tmp/physic_state.bin")?;
+    f.write_all(&serialized)?;
+
+    println!(
+        "Serialized data as bin. Saved to: physic_state.bin. hash: {}",
+        &hash
+    );
+
+    println!(
+        "\tDeterminism test: sha256(snapshot) == 24c947042ce714bc6d1125114535abbd876b8edf9e5613cf17d5379bb39d9579 : {}",
+        "24c947042ce714bc6d1125114535abbd876b8edf9e5613cf17d5379bb39d9579" == hash
+    );
+
+    Ok(())
+}
+
+fn play_with_collider_shape(
+    scene: &PhysicsState,
+    ground_collider_handle: ColliderHandle,
+    ball_collider_handle: ColliderHandle,
+) {
+    // // Shape -> 2D points
+    let tmp = &scene
+        .get_colliders()
         .get(ground_collider_handle)
         .unwrap()
         .shape()
@@ -65,7 +185,8 @@ fn main() {
 
     println!("Ground Poly: {:#?}", tmp);
 
-    let tmp = collider_set
+    let tmp = &scene
+        .get_colliders()
         .get(ball_collider_handle)
         .unwrap()
         .shape()
