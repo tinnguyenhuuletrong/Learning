668de767a1208b99ad45cd8117f6ca8a100de4a3
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2024-06-16 16:41:03 +0700

    rust - deno js runtime async support

--- rust-playground/deno_js_runtime/nodejs/node_app.js
+++ rust-playground/deno_js_runtime/nodejs/node_app.js
@@ -2,5 +2,19 @@ const path = require('path')
 const RUNTIME_PATH = '../index.node'
 const dll = require(RUNTIME_PATH)
 
-const filePath = path.resolve(__dirname, '../samples/SM_log.js')
-console.log(dll.exec_runjs(filePath))
+let count = 1
+setInterval(() => {
+    console.log('node eventLoop still free', count++)
+}, 1000);
+
+
+Promise.all([
+    dll.exec_runjs_async(path.resolve(__dirname, '../samples/fibo.js')).then(() => console.log('> \t [done] fibo')),
+    dll.exec_runjs_async(path.resolve(__dirname, '../samples/hello.js')).then(() => console.log('> \t [done] hello.js')),
+    dll.exec_runjs_async(path.resolve(__dirname, '../samples/SM_log.js')).then(() => console.log('> \t [done] SM_log')),
+    dll.exec_runjs_async(path.resolve(__dirname, '../samples/hello.ts')).then(() => console.log('> \t [done] hello.ts'))
+]).then(() => {
+    console.log('done all. Bye')
+    process.exit(0)
+})
+
--- rust-playground/deno_js_runtime/src/lib.rs
+++ rust-playground/deno_js_runtime/src/lib.rs
@@ -1,7 +1,11 @@
 use neon::prelude::*;
+use once_cell::sync::Lazy;
 use runjs_engine::run_js;
+use tokio::runtime::Runtime;
+
 mod runjs_engine;
 mod ts_loader;
+static RUNTIME: Lazy<Runtime> = Lazy::new(|| Runtime::new().unwrap());
 
 fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("hello node"))
@@ -12,21 +16,35 @@ fn exec_runjs(mut cx: FunctionContext) -> JsResult<JsUndefined> {
     let file_path_str = file_path.value(&mut cx);
     println!("Arg 0: {}", file_path_str);
 
-    let runtime = tokio::runtime::Builder::new_current_thread()
-    .enable_all()
-    .build()
-    .unwrap();
-
-    if let Err(err) = runtime.block_on(run_js(&file_path_str)) {
+    if let Err(err) = RUNTIME.block_on(run_js(&file_path_str)) {
         eprintln!("Error {err}");
     }
 
     Ok(cx.undefined())
 }
 
+fn exec_runjs_async(mut cx: FunctionContext) -> JsResult<JsPromise> {
+    let (deferred, promise) = cx.promise();
+    let file_path: Handle<JsString> = cx.argument(0)?;
+    let file_path_str = file_path.value(&mut cx);
+    println!("Arg 0: {}", file_path_str);
+
+    let channel = cx.channel();
+
+    RUNTIME.spawn_blocking(move || {
+        if let Err(err) = RUNTIME.block_on(run_js(&file_path_str)) {
+            eprintln!("Error {err}");
+        }
+        deferred.settle_with(&channel, |mut cx| Ok(cx.boolean(true)));
+    });
+
+    Ok(promise)
+}
+
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
     cx.export_function("hello", hello)?;
     cx.export_function("exec_runjs", exec_runjs)?;
+    cx.export_function("exec_runjs_async", exec_runjs_async)?;
     Ok(())
 }
