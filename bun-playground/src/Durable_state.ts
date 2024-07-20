1d9f658071981ec366d2899b15a447a175dd5e9c
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2024-07-20 16:18:53 +0700

    play with durable state

--- bun-playground/src/Durable_state.ts
+++ bun-playground/src/Durable_state.ts
@@ -1,12 +1,19 @@
+import { setTimeout } from "timers/promises";
 enum EStep {
   step_begin = "step_begin",
   step_1 = "step_1",
   step_2 = "step_2",
   step_end = "step_end",
 }
+type ContinueTrigger = {
+  type: "time";
+  resumeAt: number;
+};
 type DurableStateIterator = {
+  canContinue: boolean;
   needSave: boolean;
   activeStep: EStep;
+  resumeTrigger?: ContinueTrigger;
 };
 type DurableStateReturn = {
   isEnd: boolean;
@@ -34,15 +41,20 @@ class DurableState {
           // Move to step 1
 
           this.step = EStep.step_1;
-          yield { needSave: true, activeStep: this.step };
+          yield { canContinue: true, needSave: true, activeStep: this.step };
           break;
         }
         case EStep.step_1: {
-          // Do something
-          // Move to step 2
+          // wait for 500ms
+          yield this.pauseAndResumeAfterMs("wait_for_500ms", 500);
+          console.log("after 500ms since start");
 
+          yield this.pauseAndResumeAfterMs("wait_for_1000ms", 1000);
+          console.log("after 1500ms since start. move next");
+
+          // Move to step 2
           this.step = EStep.step_2;
-          yield { needSave: true, activeStep: this.step };
+          yield { canContinue: true, needSave: true, activeStep: this.step };
           break;
         }
         case EStep.step_2: {
@@ -62,11 +74,15 @@ class DurableState {
             );
 
             this.state["count"] = count;
-            yield { needSave: false, activeStep: this.step };
+            yield {
+              canContinue: true,
+              needSave: false,
+              activeStep: this.step,
+            };
           }
           console.log("\t", "work done");
           this.step = EStep.step_end;
-          yield { needSave: true, activeStep: this.step };
+          yield { canContinue: true, needSave: true, activeStep: this.step };
           break;
         }
         default: {
@@ -98,6 +114,34 @@ class DurableState {
     return newVal;
   }
 
+  private pauseAndResumeAfterMs(
+    key: string,
+    timeoutMs: number,
+    opt?: ExeOpt
+  ): DurableStateIterator {
+    const shouldUseCache = opt ? !opt.ignoreCache : true;
+    const cacheKey = `${this.step}:scheduler:${key}`;
+
+    if (shouldUseCache) {
+      const tmp = this.cache[cacheKey];
+      if (Date.now() >= tmp) {
+        return { canContinue: true, needSave: false, activeStep: this.step };
+      }
+    }
+
+    const resumeAt = Date.now() + timeoutMs;
+    this.cache[cacheKey] = resumeAt;
+    return {
+      canContinue: false,
+      needSave: false,
+      activeStep: this.step,
+      resumeTrigger: {
+        type: "time",
+        resumeAt,
+      },
+    };
+  }
+
   toJSON() {
     return { step: this.step, state: this.state, cache: this.cache };
   }
@@ -110,44 +154,74 @@ class DurableState {
 }
 
 async function main() {
-  const ins = new DurableState();
+  let ins = new DurableState();
   ins.step = EStep.step_begin;
 
-  let saveData: any = null;
-  console.log("----------------------------");
-  console.log("Run 1");
-  console.log("----------------------------");
-  {
-    let work = ins.exec();
-    let it = await work.next();
-    let maxIter = 3;
-    while (!it.done) {
-      it = await work.next();
-      console.log("it:", --maxIter, "->", it);
-      if (maxIter <= 0) break;
-    }
-    if (!it.done) {
-      console.log("not done");
-      saveData = ins.toJSON();
-      console.log("saved data:", saveData);
-    }
-  }
+  let preData: any = null;
+  let finalValue: any = null;
+  let count = 1;
 
-  // resume later
-  console.log("----------------------------");
-  console.log("Run 2");
-  console.log("----------------------------");
-  {
-    ins.fromJSON(saveData);
-    let work = ins.exec();
-    let it = await work.next();
-    let maxIter = 99;
-    while (!it.done) {
-      it = await work.next();
-      console.log("it:", --maxIter, "->", it);
-      if (maxIter <= 0) break;
+  while (!finalValue) {
+    console.log("----------------------------");
+    console.log("Run", count++);
+    console.log("----------------------------");
+    let maxIter = 5;
+    {
+      if (preData) {
+        // force re-construct
+        ins = new DurableState();
+        ins.fromJSON(preData);
+      }
+      const res = await runtimeRun(ins, maxIter);
+
+      // save to db somehow
+      preData = res.saveData;
+      finalValue = res.finalValue;
+
+      // handle resume
+      if (res.resumeTrigger)
+        await runtimeHandleContinueTrigger(res.resumeTrigger);
     }
-    console.log("finalValue:", it.value);
   }
+  console.log("finalValue:", finalValue);
 }
 main();
+
+async function runtimeRun(ins: DurableState, maxIter: number) {
+  let work = ins.exec();
+  let it = await work.next();
+  let resumeTrigger;
+  let saveData = null;
+  let finalValue = null;
+  while (!it.done) {
+    it = await work.next();
+    console.log("it:", --maxIter, "->", it);
+    if (!it.done && it.value.canContinue === false) {
+      // TODO: pause and register resume
+      resumeTrigger = it.value.resumeTrigger;
+      break;
+    }
+    if (maxIter <= 0) break;
+  }
+  if (!it.done) {
+    console.log("not done");
+    console.log("resumeTrigger:", resumeTrigger);
+    saveData = ins.toJSON();
+    console.log("saved data:", saveData);
+  } else {
+    finalValue = it.value;
+  }
+
+  return { saveData, finalValue, resumeTrigger };
+}
+
+async function runtimeHandleContinueTrigger(resumeTrigger: ContinueTrigger) {
+  if (resumeTrigger.type === "time") {
+    const needToSleep = resumeTrigger.resumeAt - Date.now();
+    console.log(`Handle resumeTrigger -> sleep ${needToSleep} `);
+    await setTimeout(needToSleep);
+    return;
+  }
+
+  throw new Error(`unknown resume trigger ${resumeTrigger.type}`);
+}
